2e8206ed280585a91e0b87526fe09ef72848a091
maarekua <markiyan89@gmail.com>
2020-02-06 00:32:58 +0200

    Update working001.js

--- working001.js
+++ working001.js
@@ -1,3 +1,4 @@
+const exercisesNumber = []
 const exercises = []
 function start () {
     const userInput = getInput()
@@ -7,15 +8,32 @@ function start () {
 function getInput() {
     return prompt(`
         Please choose exersice you want to execute:
-        ${exercises}`);
+        ${exercisesNumber}`);
 }
 
 function checkInput(input) {
-    if (+input !== NaN && exercises.includes(+input)) {console.log('it is working')}
+    const exercise = checkExerciseNumber(+input);
+    if (+input !== NaN && exercise > -1) {
+        let execution = run.bind(exercises[exercise]);
+        execution();
+    }
     else {
         alert('Please choose valid exercise number!');
         start();
     }
+}
+
+function checkExerciseNumber(input) {
+    return exercisesNumber.indexOf(input)
+}
+
+function run() {
+    const numOfArgs = this.execute.length
+    let userInput = prompt(`${this.name}
+        ${this.description},
+        Please provide ${numOfArgs} parameter(s) separeted by comma (,)`)
+    let input = userInput.split(",")
+    resultLogger(this.execute(+input[0], +input[1]))
 }
 
 function resultLogger(message) {
@@ -28,18 +46,26 @@ class Exercise {
         this.name = name,
         this.description = description,
         this.execute = execute,
-        exercises.push(number)
+        exercises.push(this),
+        exercisesNumber.push(number)
     }
 }
 
-const exercise88 = new Exercise(
+const exercise87 = new Exercise(
     87,
-    "Find Sum of last digits",
+    "sumOfLastDigits",
     "Natural numbers 'a, b' are given. Find sum of last digits",
-    sumOfLastNumbers
-    )
+    sumOfLastDigits
+)
 
-function sumOfLastNumbers(a, b) {
+const exercise226 = new Exercise(
+    226,
+    "multipleNumbers",
+    "Find multiple numbers",
+    multipleNumbers
+)
+
+function sumOfLastDigits(a, b) {
     const numbers = [a, b]
     const result = numbers.reduce((sum, el) => {
         return sum + +String(el).slice(-1);
@@ -47,4 +73,14 @@ function sumOfLastNumbers(a, b) {
     return result;
   }
 
-  start()
+function multipleNumbers(m, n) {
+  const result = [];
+  for (let i = 2; i < m && i < n; i++) {
+    if (m % i === 0 && n % i === 0) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+start()
